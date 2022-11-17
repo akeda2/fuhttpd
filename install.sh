1669e3f67ada76869c8735a9ea6b0354c81b2358
akeda <akeda2@gmail.com>
2022-11-17 15:26:39 +0100

    service installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Will install fuhttpd in /usr/local/bin and service in /etc/systemd/system
+
+[ -f serv_fuhttpd.py ] && sudo cp serv_fuhttpd.py /usr/local/bin/
+[ -f cert.pem ] && sudo cp cert.pem /usr/local/bin/
+[ -f key.pem ] && sudo cp key.pem /usr/local/bin/
+chmod a+r cert.pem
+chmod a+r key.pem
+[ -f fuhttpd.service ] && sudo cp fuhttpd.service /etc/systemd/system && sudo systemctl enable fuhttpd.service
--- tsfuhttpd.py
+++ tsfuhttpd.py
@@ -3,44 +3,15 @@ import socket
 import http.server
 from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
 import socketserver
-import argparse
-import platform
-import os
 
 hostname = str(socket.gethostname())
 #hostname = 'localhost'
 PORT = 38443
 HOST = hostname
-
-def isdir(string):
-    if os.path.isdir(string):
-        return string
-    else:
-        raise NotADirectoryError(string)
-def getdir(string):
-    return os.getcwd()
-
-parser = argparse.ArgumentParser(description="fuHTTPd",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('path', type=isdir, nargs='?', help="Root directory if omitted - use current")
-args = parser.parse_args()
-config = vars(args)
-print(config)
-dir = args.path
-
-'''
-if os.path.isdir(args.):
-    dir = args.file.name
-else:
-    print("NO DIRECTORY")
-    exit
-'''
-
 Handler = SimpleHTTPRequestHandler
 with http.server.ThreadingHTTPServer(('0.0.0.0',PORT), Handler) as httpd:
     print(HOST, str(PORT))
     sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     sslcontext.load_cert_chain(keyfile="key.pem", certfile="cert.pem")
     httpd.socket = sslcontext.wrap_socket(httpd.socket, server_side=True)
-    os.chdir(dir)
     httpd.serve_forever()
