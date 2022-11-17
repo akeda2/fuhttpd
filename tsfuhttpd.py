70722e90f61c2ebaed1a4c25e9d4e370558493ca
akeda2 <akeda2@users.noreply.github.com>
2022-11-17 14:17:55 +0100

    parse test

--- tsfuhttpd.py
+++ tsfuhttpd.py
@@ -3,11 +3,28 @@ import socket
 import http.server
 from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
 import socketserver
+import argparse
+import platform
+import os
 
 hostname = str(socket.gethostname())
 #hostname = 'localhost'
 PORT = 38443
 HOST = hostname
+
+parser = argparse.ArgumentParser(description="fuHTTPd",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('file', default=None, nargs='?', type=argparse.FileType('r'), help="Root directory, if omitted - use current")
+args = parser.parse_args()
+config = vars(args)
+print(config)
+
+if os.path.isdir(args.file.name):
+    directory = args.file.name
+else:
+    print("NO DIRECTORY")
+    exit
+
 Handler = SimpleHTTPRequestHandler
 with http.server.ThreadingHTTPServer(('0.0.0.0',PORT), Handler) as httpd:
     print(HOST, str(PORT))
