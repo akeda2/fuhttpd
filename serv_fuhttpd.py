de50ad803dba757d4f115e9dd0e0be5d1e075977
davake01 <david.akesson@sr.se>
2022-11-18 09:55:36 +0100

    optional plain http

--- serv_fuhttpd.py
+++ serv_fuhttpd.py
@@ -9,9 +9,12 @@ import platform
 import os
 
 hostname = str(socket.gethostname())
-#hostname = 'localhost'
+httpsport=38443
+httpport=38080
 PORT = 38443
 HOST = hostname
+certpath=str(os.path.realpath("cert.pem"))
+keypath=str(os.path.realpath("key.pem"))
 
 def isdir(string):
     if os.path.isdir(string):
@@ -24,6 +27,7 @@ def getdir(string):
 parser = argparse.ArgumentParser(description="fuHTTPd",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('path', type=isdir, nargs='?', help="Root directory if omitted - use current")
+parser.add_argument('-p', '--plain', action='store_true', default=False, help="Use plain HTTP")
 args = parser.parse_args()
 config = vars(args)
 print(config)
@@ -37,11 +41,23 @@ else:
     exit
 '''
 
+
 Handler = SimpleHTTPRequestHandler
-with http.server.ThreadingHTTPServer(('0.0.0.0',PORT), Handler) as httpd:
-    print(HOST, str(PORT))
-    sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    sslcontext.load_cert_chain(keyfile="/usr/local/bin/key.pem", certfile="/usr/local/bin/cert.pem")
-    httpd.socket = sslcontext.wrap_socket(httpd.socket, server_side=True)
-    os.chdir(dir)
-    httpd.serve_forever()
+
+if args.plain:
+    PORT = httpport
+    with socketserver.ThreadingTCPServer(("", PORT), Handler) as phttpd:
+        print(HOST, str(PORT))
+        if args.path:
+            os.chdir(dir)
+        phttpd.serve_forever()
+else:
+    PORT = httpsport
+    with http.server.ThreadingHTTPServer(('0.0.0.0',PORT), Handler) as httpd:
+        print(HOST, str(PORT))
+        sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        sslcontext.load_cert_chain(keyfile=keypath, certfile=certpath)
+        httpd.socket = sslcontext.wrap_socket(httpd.socket, server_side=True)
+        if args.path:
+            os.chdir(dir)
+        httpd.serve_forever()
