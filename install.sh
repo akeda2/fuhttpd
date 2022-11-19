c0044689392c6fb567cd4b6ba4ed4092eb059132
akeda <akeda2@gmail.com>
2022-11-19 13:25:27 +0100

    installer updated

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # Will install fuhttpd in /usr/local/bin and service in /etc/systemd/system
 
 [ -f fuhttpd.py ] && sudo cp fuhttpd.py /usr/local/bin/
-[ -f cert.pem ] && sudo cp cert.pem /usr/local/bin/
-[ -f key.pem ] && sudo cp key.pem /usr/local/bin/
-chmod a+r cert.pem
-chmod a+r key.pem
-[ -f fuhttpd.service ] && sudo cp fuhttpd.service /etc/systemd/system && sudo systemctl enable fuhttpd.service
+[ -f cert.pem ] && sudo cp cert.pem /usr/local/bin/ || echo "No cert.pem, please generate!"
+[ -f key.pem ] && sudo cp key.pem /usr/local/bin/ || echo "No key.pem, please generate!"
+chmod a+r cert.pem || echo "No cert.pem, please generate!"
+chmod a+r key.pem || echo "No key.pem, please generate!"
+[ -f fuhttpd.service ] && sudo cp fuhttpd.service /etc/systemd/system && sudo systemctl enable fuhttpd.service && echo "Service enabled, run systemctl start fuhttpd, to start"
