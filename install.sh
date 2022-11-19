4c0c94c8052607c721d98acc08d98de8fe59387b
akeda <akeda2@gmail.com>
2022-11-19 13:41:09 +0100

    installer updated, working

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 #
 # Will install fuhttpd in /usr/local/bin and service in /etc/systemd/system
 
-[ -f fuhttpd.py ] && sudo cp fuhttpd.py /usr/local/bin/
-[ -f cert.pem ] && sudo cp cert.pem /usr/local/bin/ || echo "No cert.pem, please generate!"
-[ -f key.pem ] && sudo cp key.pem /usr/local/bin/ || echo "No key.pem, please generate!"
-chmod a+r cert.pem || echo "No cert.pem, please generate!"
-chmod a+r key.pem || echo "No key.pem, please generate!"
+INSTALLDIR="/usr/local/bin"
+
+[ -f fuhttpd.py ] && sudo cp fuhttpd.py "$INSTALLDIR" || echo "fuhttpd.py not found! Where are we running this from?"
+[ -f cert.pem ] && sudo cp cert.pem "$INSTALLDIR" || echo "No cert.pem, please generate!"
+[ -f key.pem ] && sudo cp key.pem "$INSTALLDIR" || echo "No key.pem, please generate!"
+sudo chmod a+r "$INSTALLDIR"/cert.pem || echo "No cert.pem, please generate!"
+sudo chmod a+r "$INSTALLDIR"/key.pem || echo "No key.pem, please generate!"
+sudo useradd --system --shell=/usr/sbin/nologin www-data
 [ -f fuhttpd.service ] && sudo cp fuhttpd.service /etc/systemd/system && sudo systemctl enable fuhttpd.service && echo "Service enabled, run systemctl start fuhttpd, to start"
