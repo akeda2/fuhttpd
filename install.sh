0ddd09c6196f35c1ede59797dcbcb9e67304e7c8
akeda <akeda2@gmail.com>
2022-12-09 19:32:19 +0100

    no backslash

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ cont () {
 	else
 		prompt="$1"
 	fi
-        read -p "$prompt\? (y/n): " -n 1 -r
+        read -p "$prompt? (y/n): " -n 1 -r
         echo    # (optional) move to a new line
         if [[ $REPLY =~ ^[Yy]$ ]]; then
                 #exit 0 #
