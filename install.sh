0505b29a95c95b8957c3af10ae116f90d7432419
akeda2 <akeda2@users.noreply.github.com>
2022-12-09 13:32:02 +0100

    Installer cleaned up

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ WEBROOT="/var/www/html/"
 cont () {
         #Reads from input and returns true/false
         # Ex: cont && do something
-        [ -z "$1" ] && 1="Continue?"
+#        [ -z "$1" ] && 1="Continue?"
         read -p "$1? (y/n): " -n 1 -r
         echo    # (optional) move to a new line
         if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -21,13 +21,19 @@ INSTALLDIR="/usr/local/bin"
 [ -f fuhttpd.py ] && sudo cp fuhttpd.py "$INSTALLDIR" || echo "fuhttpd.py not found! Where are we running this from?"
 [ -f cert.pem ] && sudo cp cert.pem "$INSTALLDIR" || echo "No cert.pem, please generate!"
 [ -f key.pem ] && sudo cp key.pem "$INSTALLDIR" || echo "No key.pem, please generate!"
-sudo chmod a+r "$INSTALLDIR"/cert.pem || echo "No cert.pem, please generate!"
-sudo chmod a+r "$INSTALLDIR"/key.pem || echo "No key.pem, please generate!"
+
 sudo useradd --system --shell=/usr/sbin/nologin www-data || echo "ERROR! User already exists...?"
+
+sudo chown www-data:www-data "$INSTALLDIR"/cert.pem && sudo chmod 640 "$INSTALLDIR"/cert.pem || echo "No cert.pem, please generate!"
+sudo chown www-data:www-data "$INSTALLDIR"/key.pem && sudo chmod 640 "$INSTALLDIR"/key.pem || echo "No key.pem, please generate!"
+
+
 sudo systemctl stop fuhttpd.service && echo "Old service stopped!" || echo "No service stopped"
+
 [ ! -d "$WEBROOT" ] && echo "Default webroot: "$WEBROOT" does not exist!" && cont "Create?" && sudo mkdir -p "$WEBROOT" && sudo chown -R www-data:www-data "$WEBROOT"
+
 echo "Do you want to run HTTPS, plain HTTP or both?"
-#echo "Activate HTTPS on 443?"
-cont "Activate HTTPS on 443?" && [ -f fuhttpd.service ] && sudo cp fuhttpd.service /etc/systemd/system && sudo systemctl daemon-reload && sudo systemctl enable fuhttpd.service && echo "Service enabled. Run systemctl start fuhttpd, to start." && cont "Start service now?" && sudo systemctl start fuhttpd.service
-#echo "Activate plain HTTP on 80?"
-cont "Activate plain HTTP on 80?" && [ -f plain-fuhttpd.service ] && sudo cp plain-fuhttpd.service /etc/systemd/system && sudo systemctl daemon-reload && sudo systemctl enable plain-fuhttpd.service && echo "Service enabled. Run systemctl start fuhttpd, to start." && cont "Start service now?" && sudo systemctl start plain-fuhttpd.service
+
+cont "Activate HTTPS on 443" && [ -f fuhttpd.service ] && sudo cp fuhttpd.service /etc/systemd/system && sudo systemctl daemon-reload && sudo systemctl enable fuhttpd.service && echo "Service enabled. Run systemctl start fuhttpd, to start." && cont "Start service now" && sudo systemctl start fuhttpd.service
+
+cont "Activate plain HTTP on 80" && [ -f plain-fuhttpd.service ] && sudo cp plain-fuhttpd.service /etc/systemd/system && sudo systemctl daemon-reload && sudo systemctl enable plain-fuhttpd.service && echo "Service enabled. Run systemctl start fuhttpd, to start." && cont "Start service now" && sudo systemctl start plain-fuhttpd.service
