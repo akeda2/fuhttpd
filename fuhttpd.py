33af29bc4551e557f6dda72b67d9a078608ea502
akeda <akeda2@gmail.com>
2023-03-01 23:15:40 +0100

    working with yaml config file

--- fuhttpd.py
+++ fuhttpd.py
@@ -8,118 +8,139 @@ import argparse
 import platform
 import os
 from multiprocessing import Process
+import yaml
+import signal
+import sys
 
-hostname = str(socket.gethostname())
-HOST = hostname
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
-# Default ports:
-httpsport=38443
-httpport=38080
-PORT = httpsport
+try:
+    hostname = str(socket.gethostname())
+    HOST = hostname
 
-def isdir(string):
-    if os.path.isdir(string):
-        return string
+    # Default ports:
+    httpsport=38443
+    httpport=38080
+    PORT = httpsport
+
+    # Default paths:
+    certpath = str(os.path.realpath("/usr/local/share/ca-certificates/cert.pem"))
+    keypath = str(os.path.realpath("/usr/local/share/ca-certificates/key.pem"))
+
+    def isdir(string):
+        if os.path.isdir(string):
+            return string
+        else:
+            raise NotADirectoryError(string)
+    def getdir(string):
+        return os.getcwd()
+
+    parser = argparse.ArgumentParser(description="fuHTTPd",
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('path', type=isdir, nargs='?', help="Root directory. If omitted - use current")
+    parser.add_argument('-P', '--plain', action='store_true', default=False, help="Use plain HTTP")
+    parser.add_argument('-p', '--port', type=int, help="Use custom port")
+    parser.add_argument('-d', '--dirlist', action='store_true', default=False, help="Allow directory listings")
+    parser.add_argument('-c', '--cert', type=str, help="Use certificate")
+    parser.add_argument('-k', '--key', type=str, help="Use key")
+    parser.add_argument('-s', '--save', action='store_true', default=False, help="Save config to file")
+    parser.add_argument('-l', '--load', nargs='?', const='.fuhttpd.yaml', help="Load config from file")
+    args = parser.parse_args()
+    config = vars(args)
+    print(config)
+    
+
+    # Load config from yaml file:
+    if args.load and not args.save:
+        print(args.load)
+        config_path = os.path.normpath('.fuhttpd.yaml')# if args.load else os.path.normpath('.fuhttpd.yaml')
+
+        with open(config_path, 'r') as f:
+            config = yaml.safe_load(f)
+        certpath = config['cert']
+        keypath = config['key']
+        args.plain = config['plain']
+        args.dirlist = config['dirlist']
+        args.path = config['path']
+        args.port = config['port']
+        print(config)
+    dir = args.path
+    # Save config to yaml file:
+    if args.save:
+        """if not args.dirlist:
+            args.dirlist = input("Allow directory listings? (y/N): ") == 'y'
+            print(str(args.dirlist))
+            if args.dirlist == True:
+                args.dirlist = True"""
+        new_config = {
+            'path': args.path or input("Path/Webroot: " + os.getcwd()) or os.getcwd(),
+            'plain': args.plain or input("Use plain HTTP? (y/N): ") == 'y',
+            'port': args.port or input('Port: (' + str(PORT) + ')') or PORT if not args.plain else input('Port: (' + str(httpport) + ')') or httpport,
+            'dirlist': args.dirlist or input("Allow directory listings? (y/N): ") == 'y',
+            'cert': args.cert or input("Certificate: (" + certpath + ")") or certpath,
+            'key': args.key or input("Key: (" + keypath + ")") or keypath,
+        }
+        with open('.fuhttpd.yaml', 'w') as f:
+            yaml.dump(new_config, f)
+        exit(0)
+
+
+    # Do we want to allow directory listing? If so, use default handler settings.
+    if not args.dirlist:
+        class Handler(SimpleHTTPRequestHandler):
+    #        def do_LIST(self):
+    #            pass
+            def list_directory(self, path):
+                self.send_error(404, "File not found")
     else:
-        raise NotADirectoryError(string)
-def getdir(string):
-    return os.getcwd()
+        Handler = SimpleHTTPRequestHandler
 
-parser = argparse.ArgumentParser(description="fuHTTPd",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('path', type=isdir, nargs='?', help="Root directory. If omitted - use current")
-parser.add_argument('-P', '--plain', action='store_true', default=False, help="Use plain HTTP")
-parser.add_argument('-p', '--port', type=int, help="Use custom port")
-parser.add_argument('-d', '--dirlist', action='store_true', default=False, help="Allow directory listings")
-args = parser.parse_args()
-config = vars(args)
-print(config)
-dir = args.path
+    # This is changed! Certs are in /usr/local/share/ca-certificates/ as default!
+    # Cert and key should be in the WorkingDirectory (default: /usr/local/bin/) - NOT in webroot!
+    # I may change this and add an option for custom placement, but I don't need it now...
+    """if not args.plain:
+        certpath = os.path.normpath(args.cert) if args.cert else str(os.path.realpath("cert.pem")) 
+        keypath = os.path.normpath(args.key) if args.key else str(os.path.realpath("key.pem"))"""
 
-# Do we want to allow directory listing? If so, use default handler settings.
-if not args.dirlist:
-    class Handler(SimpleHTTPRequestHandler):
-#        def do_LIST(self):
-#            pass
-        def list_directory(self, path):
-            self.send_error(404, "File not found")
-else:
-    Handler = SimpleHTTPRequestHandler
-# If not, override with 404 on '/'-requests
-#    class Handler(SimpleHTTPRequestHandler):
-#        def translate_path(self, path):
-        # Check if the request path is a directory
-#            if os.path.isdir(path):
-                # Check if the directory is empty
-                #if not os.listdir(path):
-                    # If the directory is empty, serve index.html
-#                return os.path.join(path, 'index.html')
-                #else:
-                    # If the directory is not empty, return an error
-                 #   raise ValueError("Directory listings are not allowed")
+    if args.plain:
+        # -P Plain HTTP server with no encryption
+        if args.port:
+            PORT = args.port
+        else:
+            PORT = httpport
+        # Replaced socketserver.ThreadingTCPServer
+        with http.server.ThreadingHTTPServer(('0.0.0.0', PORT), Handler) as phttpd:
+            print(HOST, str(PORT))
+            if args.path:
+                os.chdir(dir)
+    #        phttpd.serve_forever()
 
-            # Otherwise, continue with the default behavior
-#            else:
-#                return super().translate_path(path)
-
-#        def do_GET(self):
-#            if os.path.isdir(self.path):
-#                # Check for index.html
-#                index_path = os.path.join(os.getcwd(), self.path, 'index.html')
-#		print(index_path)
-#                if os.path.exists(index_path):
-#                    self.path = index_path
-#                else:
-#                    self.send_error(404, index_path)#"Directory listings are not allowed")
-#                    return
-
-            # If the request is for a directory, return a 404 "Not Found" response
-#            if self.path.endswith("/"):
-#                self.send_error(404, "Not Found")
-#                return
-        
-            # Otherwise, use the default behavior for handling requests for files
-#            super().do_GET()
-
-# Cert and key should be in the WorkingDirectory (default: /usr/local/bin/) - NOT in webroot!
-# I may change this and add an option for custom placement, but I don't need it now...
-if not args.plain:
-    certpath=str(os.path.realpath("cert.pem"))
-    keypath=str(os.path.realpath("key.pem"))
-
-if args.plain:
-    # -P Plain HTTP server with no encryption
-    if args.port:
-        PORT = args.port
+        # Create a process for each CPU core (or SMT thread):
+            for i in range(os.cpu_count()):
+                p = Process(target=phttpd.serve_forever)
+                p.start()
     else:
-        PORT = httpport
-    # Replaced socketserver.ThreadingTCPServer
-    with http.server.ThreadingHTTPServer(('0.0.0.0', PORT), Handler) as phttpd:
-        print(HOST, str(PORT))
-        if args.path:
-            os.chdir(dir)
-#        phttpd.serve_forever()
+        # HTTPS using ssl.SSLContext.wrap_socket():
+        if args.port:
+            PORT = args.port
+        else:
+            PORT = httpsport
+        with http.server.ThreadingHTTPServer(('0.0.0.0',PORT), Handler) as httpd:
+            print(HOST, str(PORT))
+            sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            sslcontext.load_cert_chain(keyfile=keypath, certfile=certpath)
+            httpd.socket = sslcontext.wrap_socket(httpd.socket, server_side=True)
+            if args.path:
+                os.chdir(dir)
+    #        httpd.serve_forever()
 
-    # Create a process for each CPU core (or SMT thread):
-        for i in range(os.cpu_count()):
-            p = Process(target=phttpd.serve_forever)
-            p.start()
-else:
-    # HTTPS using ssl.SSLContext.wrap_socket():
-    if args.port:
-        PORT = args.port
-    else:
-        PORT = httpsport
-    with http.server.ThreadingHTTPServer(('0.0.0.0',PORT), Handler) as httpd:
-        print(HOST, str(PORT))
-        sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-        sslcontext.load_cert_chain(keyfile=keypath, certfile=certpath)
-        httpd.socket = sslcontext.wrap_socket(httpd.socket, server_side=True)
-        if args.path:
-            os.chdir(dir)
-#        httpd.serve_forever()
-
-    # Create a process for each CPU core (or SMT thread):
-        for i in range(os.cpu_count()):
-            p = Process(target=httpd.serve_forever)
-            p.start()
+        # Create a process for each CPU core (or SMT thread):
+            for i in range(os.cpu_count()):
+                p = Process(target=httpd.serve_forever)
+                p.start()
+except KeyboardInterrupt:
+    print("KeyboardInterrupt")
+    sys.exit(0)
--- generate-keys.sh
+++ generate-keys.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Generate a pair of generic ssl cert and keys
 openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365
+# Copy the cert and key to /usr/local/share/ca-certificates:
+sudo cp cert.pem /usr/local/share/ca-certificates/
+sudo cp key.pem /usr/local/share/ca-certificates/
